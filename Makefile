ddea26d6ab4a084f7be9104a0da67364dca17843
Nataliia Petrushak <amirammina@gmail.com>
2021-11-01 19:00:47 +0200

    Was made my first game

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
-install: # installing npm ci   
+install: # installing npm ci
 	npm ci
 brain-games: # запуск программы
 	node bin/brain-games.js
+brain-even: # запуск brain-even.js
+	node bin/brain-even.js
 publish: #npm publish --dry-run
 	npm publish --dry-run
 lint: #use linter
---
+++ bin/brain-even.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+import cli from '../src/cli.js';
+
+function getRandomNumbers(min, max) {
+  const number = Math.random() * (max - min) + min;
+  return Math.floor(number);
+}
+
+const parityCheck = () => {
+  const name = cli();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  for (let i = 0; i < 3; i += 1) {
+    const randomNumber = getRandomNumbers(1, 30);
+    console.log(`Question: ${randomNumber}`);
+    const answer = readlineSync.question('Your answer: ');
+    if ((randomNumber % 2 === 0 && answer === 'yes')
+      || (randomNumber % 2 !== 0 && answer === 'no')) {
+      console.log('Correct!');
+    } else if (randomNumber % 2 === 0 && answer !== 'yes') {
+      console.log(`${answer} is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}!`);
+      return;
+    } else if (randomNumber % 2 !== 0 && answer !== 'no') {
+      console.log(`${answer} is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+
+parityCheck();
