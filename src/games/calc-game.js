86b23995d919f1f98b7e1152b14e915fc021bee0
Nataliia Petrushak <amirammina@gmail.com>
2021-11-15 01:22:05 +0200

    fix eslint problems

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -5,20 +5,20 @@ const operands = ['+', '-', '*'];
 
 const rule = 'What is the result of the expression?';
 const getExpressionAndAnswer = () => {
-    const randomNumber1 = getRandomNumbers(1, 50);
-    const randomNumber2 = getRandomNumbers(1, 50);
-    const operand = operands[getRandomNumbers(0, 3)];
-    const expression = `${randomNumber1} ${operand} ${randomNumber2}`;
-    let rightAnswer = 0;
-    if (operand === '+') {
-        rightAnswer = randomNumber1 + randomNumber2;
-    } else if (operand === '-') {
-        rightAnswer = randomNumber1 - randomNumber2;
-    } else if (operand === '*') {
-        rightAnswer = randomNumber1 * randomNumber2;
-    }
-    rightAnswer = rightAnswer.toString();
-    return [expression, rightAnswer];
+  const randomNumber1 = getRandomNumbers(1, 50);
+  const randomNumber2 = getRandomNumbers(1, 50);
+  const operand = operands[getRandomNumbers(0, 3)];
+  const expression = `${randomNumber1} ${operand} ${randomNumber2}`;
+  let rightAnswer = 0;
+  if (operand === '+') {
+    rightAnswer = randomNumber1 + randomNumber2;
+  } else if (operand === '-') {
+    rightAnswer = randomNumber1 - randomNumber2;
+  } else if (operand === '*') {
+    rightAnswer = randomNumber1 * randomNumber2;
+  }
+  rightAnswer = rightAnswer.toString();
+  return [expression, rightAnswer];
 };
 
 const Calc = () => game(rule, getExpressionAndAnswer);
--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -4,23 +4,23 @@ import game from '../index.js';
 const rule = 'Find the greatest common divisor of given numbers.';
 
 const gcd = () => {
-    const firstNumber = getRandomNumbers(0, 100);
-    const secondNumber = getRandomNumbers(0, 100);
-    let theSmallestNumber = 0;
-    if (firstNumber < secondNumber) {
-        theSmallestNumber = firstNumber;
-    } else {
-        theSmallestNumber = secondNumber;
+  const firstNumber = getRandomNumbers(0, 100);
+  const secondNumber = getRandomNumbers(0, 100);
+  let theSmallestNumber = 0;
+  if (firstNumber < secondNumber) {
+    theSmallestNumber = firstNumber;
+  } else {
+    theSmallestNumber = secondNumber;
+  }
+  const expression = `${firstNumber} ${secondNumber}`;
+  let rightAnswer = '';
+  for (let i = 0; i <= theSmallestNumber; i += 1) {
+    if (firstNumber % i === 0 && secondNumber % i === 0) {
+      rightAnswer = i;
     }
-    const expression = `${firstNumber} ${secondNumber}`;
-    let rightAnswer = '';
-    for (let i = 0; i <= theSmallestNumber; i += 1) {
-        if (firstNumber % i === 0 && secondNumber % i === 0) {
-            rightAnswer = i;
-        }
-    }
-    rightAnswer = rightAnswer.toString();
-    return [expression, rightAnswer];
+  }
+  rightAnswer = rightAnswer.toString();
+  return [expression, rightAnswer];
 };
 
 const gcdGame = () => game(rule, gcd);
